0678bda7eb2a3508959f9a54859a23259cd8a1b9
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2021-03-07 20:54:15 -0800

    Implementando estructura 2

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ from Functions import *
 
 memoryAmount = random.randint(1, 10)
 instructions = random.randint(1, 10)
-# ready = random.randint(1, 2)
+
 
 # Programa que se ejecutará
 def program(env):
@@ -29,54 +29,69 @@ def program(env):
 class SystemOperative(object):
 
     # --> Constructor y Atributos
-    def __init__(self, env, ram, data) -> None:
+    def __init__(self, env) -> None:
         self.env = env
         self.action = env.process(self.newProcess())
-        self.ram = ram
-        self.data = data
+        self.RAM = simpy.Container(env, init=100, capacity=100)
+        self.data = []
         self.instructionsCount = 0
 
     # --> Métodos
     # Si hay memoria disponible puede pasar al estado "Ready"
     # Si no se pasará haciendo cola
-    def newProcess(self):        
-        # Memoria al azar (del 1 al 10)
-        yield env.timeout(memoryAmount)
-        # Empezar a hacer el proceso
-        print("Ejecutando proceso con " + str(env.now) + " de memoria")
-        # Para verificar si hay memoria disponible
-        if(memoryAmount == 10): # Lleno
-            # Se estará esperando por memoria y permanece haciendo cola
-            self.waiting()
-        else:
-            getReady = env.process(self.ready(env))
+    def newProcess(self):   
+        while True:
+            # print("Capacidad RAM: " + str(self.RAM.capacity))     
+            # Memoria al azar (del 1 al 10)
+            self.RAM.get(100)
+            yield env.timeout(self.instructionsCount)
+            # Empezar a hacer el proceso
+            print("Ejecutando proceso con " + str(env.now) + " de memoria")
+
+            # Para verificar si hay memoria disponible
+            if(self.RAM.level <= 0): # Lleno
+                print("Lleno Xd")
+                # Se estará esperando por memoria y permanece haciendo cola
+                getWaiting = env.process(self.waiting(env))
+            else: # Disponible
+                print("Disponible ;)")
+                getReady = env.process(self.ready(env))
         
     # Esperar para que el CPU nos Atienda
     def ready(self, env):
-        yield env.timeout(instructions)
-        # self.instructionsCount += instructions
         print("Instruccioens a realizar: " + str(env.now))  
+        self.instructionsCount += 1
+        yield env.timeout(self.instructionsCount)
 
     # El CPU atiende al proceso por un tiempo limitado, suficiente para realizar solamente 3 instrucciones
     # (Se debe de actualizar el contador de instrucciones a realizar)
-    def running(self):
+    def running(self, env):
         print("")
 
     # Para concluir con el proceso
-    def terminated(self):
+    def terminated(self, env):
         print("")
 
     # Para esperar, se generaran numero al azar entre 1 y 2
-    # 1- Para pasar a la cola de Waiting
-    # 2- Para dejar esa cola y regresa a "ready"
-    def waiting(self):
-        print("")
+    # 1- Estará en la cola de Waiting (Atras)
+    # 2- Para dejar esa cola y regresa a "ready" (Adelante )
+    def waiting(self, env):
+        ready = random.randint(1, 2)
+        if(ready == 1):
+            print("Waiting")
+            print("Colas: " + str(env.now))
+            self.instructionsCount += 1
+            yield env.timeout(self.instructionsCount)
+        else:
+            print("Ready")
+            print("Instruccioens a realizar: " + str(env.now))
+            self.instructionsCount += 1
+            yield env.timeout(self.instructionsCount)
 
 if __name__ == "__main__":
     
-    data = []   
     env = simpy.Environment()
-    RAM = simpy.Container(env, init=100, capacity=100)
-    systemO = SystemOperative(env, RAM, data)
-    env.run()
+    systemO = SystemOperative(env)
+    PROCESSES = 5 # Para realizar los procesos solicitados
+    env.run(until=PROCESSES)
     
